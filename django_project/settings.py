86ddc028705e47a9626766fe3b3ead51dca31d08
Muhammad Hashim <89855559+HashimThePassionate@users.noreply.github.com>
2024-10-08 00:42:17 +0500

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -93,8 +93,12 @@ LOGOUT_REDIRECT_URL = "home"  # new
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
